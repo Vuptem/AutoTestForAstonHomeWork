b4c875892c625a08f261cf5d4bbf90b5c722c15b
Vuptem <nshatoha1995@mail.ru>
2025-09-13 23:06:16 +0300

    test(junit5): add tests for factorial, triangle, calc, comparator

---
+++ src/main/java/Lesson_2_7/IntComparator.java
@@ -0,0 +1,8 @@
+package Lesson_2_7;
+
+public class IntComparator {
+    /** -1 если a<b; 0 если a==b; 1 если a>b */
+    public static int compare(int a, int b) {
+        return Integer.compare(a,b);
+    }
+}
